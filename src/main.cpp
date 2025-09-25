0d11568093798bcd0f52c44c67ba38c715eff959
Vansh <veervansh07@gmail.com>
2025-09-25 13:11:27 +0200

    Test v1

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,13 @@
 #include <Arduino.h>
-#include <QTRSensors.h> // for IR sensoren
+#include <QTRSensors.h> // kan være denne ikke funker ordentlig
 
-//Motor class
 class Motor {
     //Hver motor trenger 3 pins, in1 og in2 er fremover/bakover mens pwm kontroller fart
     int pwmPin, in1, in2;
 
 public:
-    Motor(int pwmPin, int in1, int in2) {
-        in1 = pin1;
-        in2 = pin2;
+    Motor(int pwmPin, int pin1, int pin2) : pwmPin(pwmPin), in1(pin1), in2(pin2) {
+
         pinMode(pwmPin, OUTPUT);
         pinMode(in1, OUTPUT);
         pinMode(in2, OUTPUT);
@@ -29,6 +27,7 @@ public:
             // Slik at fart holder seg innenfor rekkevidden 0-255
             speed = -speed;
         }
+        analogWrite(pwmPin, speed);
     }
 };
 
@@ -40,8 +39,7 @@ class MotorDriver {
 
 public:
     //Member initializer list, kopierer to motor objekter in til dette MotorDriver objektet
-    MotorDriver(int stbyPin, Motor l,Motor r) : leftMotor(l), rightMotor(r) {
-        stbyPin = stby;
+    MotorDriver(int stbyPin, Motor l,Motor r) : stbyPin(stbyPin), leftMotor(l), rightMotor(r) {
         pinMode(stbyPin, OUTPUT);
         digitalWrite(stbyPin, HIGH); //Skru på driver
     }
@@ -50,10 +48,109 @@ public:
         rightMotor.drive(rightSpeed);
     }
 };
+// koden er tatt fra QTRSensors sin eksempeloppgave
+
+QTRSensors qtr;
+
+const uint8_t sensor_count = 6;
+uint16_t sensor_values[sensor_count];
+
+Motor leftMotor(9,10,11);
+Motor rightMotor(5,6,7);
+MotorDriver driver(8, leftMotor, rightMotor);
+
 void setup() {
 // write your initialization code here
+    Serial.begin(9600);
+
+    qtr.setTypeRC(); // her kunne man også ha brukt setTypeAnalog() men RC (Resistor-Capacitor) skal visst være raskere
+    qtr.setSensorPins((const uint8_t[]){A0, A1, A2, A3, A4, A5}, sensor_count); // bruk disse pinsene på arduinoen
+    qtr.setEmitterPin(2);
+
+
+    delay(500);
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, HIGH); // turn on Arduino's LED to indicate we are in calibration mode
+
+    Serial.println("Starter QTRs Sensor-kalibrering.");
+    // analogRead() takes about 0.1 ms on an AVR.
+    // 0.1 ms per sensor * 4 samples per sensor read (default) * 6 sensors
+    // * 10 reads per calibrate() call = ~24 ms per calibrate() call.
+    // Call calibrate() 400 times to make calibration take about 10 seconds.
+    for (uint16_t i = 0; i < 400; i++)
+    {
+        Serial.println(i);
+        qtr.calibrate();
+    }
+
+    Serial.println("Ferdig med kalibrering.");
+    digitalWrite(LED_BUILTIN, LOW); // signalisere at vi er ferdig å kalibrere
+
+    // print the calibration minimum values measured when emitters were on
+    Serial.begin(9600);
+    for (uint8_t i = 0; i < sensor_count; i++)
+    {
+        Serial.print(qtr.calibrationOn.minimum[i]);
+        Serial.print(' ');
+    }
+    Serial.println();
+
+    // print the calibration maximum values measured when emitters were on
+    for (uint8_t i = 0; i < sensor_count; i++)
+    {
+        Serial.print(qtr.calibrationOn.maximum[i]);
+        Serial.print(' ');
+    }
+    Serial.println();
+    Serial.println();
+    delay(1000);
 }
+const int baseSpeed = 100;
 
 void loop() {
 // write your code here
+    // read calibrated sensor values and obtain a measure of the line position
+    // from 0 to 5000 (for a white line, use readLineWhite() instead)
+    uint16_t position = qtr.readLineBlack(sensor_values);
+
+    // print the sensor values as numbers from 0 to 1000, where 0 means maximum
+    // reflectance and 1000 means minimum reflectance, followed by the line
+    // position
+    for (uint8_t i = 0; i < sensor_count; i++)
+    {
+        Serial.print(sensor_values[i]);
+        Serial.print('\t');
+    }
+    Serial.println(position);
+
+    delay(250);
+
+    int leftSpeed = baseSpeed;
+    int rightSpeed = baseSpeed;
+
+    leftSpeed = constrain(leftSpeed, 0, 255);
+    rightSpeed = constrain(rightSpeed, 0, 255);
+
+    driver.move(leftSpeed, rightSpeed);
+
+    delay(10);
 }
+
+/*
+ *      Forventet output:
+ *      Starter QTRs Sensor-kalibrering.
+ *      1
+ *      2
+ *      3
+ *      ...
+ *      399
+ *      Ferdig med kalibrering.
+ *
+ *      392 456 429 489 389 289         // Dette tallet vil vi skal være lavt (det er laveste målingene
+ *                                      // under kalibreringen
+ *      2450 2500 2500 2500 2500 2499   // dette ønsker vi skal være på så nærme 2500 som mulig (høyeste
+ *                                      // målingene under kalibreringen)
+ *      0 0 0 0 0 0 0
+ *      800 90 0 0 0 0 300 <----- de 6 første verdiene er hver sensor sin data (over svar linje betyr høy verdi)
+ *      0 0 102 708 302 80 2000 <--- den 7 verdien er posisjonen verdi (hvor den svarte linjen er på sensoren)
+ */
